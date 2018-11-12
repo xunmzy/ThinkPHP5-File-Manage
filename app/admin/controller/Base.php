c218941a21ed766faaafa4f5050608b6fba657d1
寻梦 <83525961@qq.com>
2018-11-12 09:51:35 -0500

    ThinkPHP5文件管理系统

---
+++ app/admin/controller/Base.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * User: Xunm
+ * Date: 2018/10/10
+ * Time: 1:01
+ */
+
+namespace app\admin\controller;
+
+
+use think\Controller;
+
+class Base extends Controller
+{
+}
---
+++ runtime/temp/88d101f835c596f15d2356f3cc067b09.php
@@ -0,0 +1,11 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:51:"/data/www/public/../app/index/view/index/index.html";i:1539392716;}*/ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+46549798789
+</body>
+</html>
